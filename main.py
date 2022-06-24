baffe91f659eb181fab82ec89d875cc9c62b1037
plentakill5h <105579392+plentakill5h@users.noreply.github.com>
2022-06-24 06:29:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from http import client
 import webbrowser
 import selenium
 from selenium import webdriver
@@ -11,6 +12,9 @@ with open('/home/ubuntu/Desktop/telegram_bot/files/groups.txt','r') as groupsLis
 login_url = "https://web.telegram.org/?legacy=1#/login"
 driver = webdriver.Chrome("/usr/bin/chromedriver")
 driver.get(login_url)
+
+
+users_list = groups
 
 
 print("Connecting to Telegram Web, please wait")
@@ -36,6 +40,28 @@ time.sleep(3)
 enter_code_input = driver.find_element_by_xpath("//*[@id='ng-app']/body/div[1]/div/div[3]/div[2]/form/div[3]/input")
 enter_code = input("Code: ")
 
-time.sleep(50)
+time.sleep(25)
 
 enter_code_input.send_keys(enter_code + Keys.ENTER)
+
+time.sleep(3)
+
+search_message = driver.find_element_by_xpath("//*[@id='ng-app']/body/div[1]/div[2]/div/div[1]/div[1]/div/input")
+
+time.sleep(3)
+
+#search_message.send_keys("@drake")
+
+text = "last seen a long time ago"
+
+async def main():
+    for i in range(len(groups)):
+
+        search_message.send_keys(users_list[i] + Keys.ENTER)
+        time.sleep(3)
+
+        if text in driver.page_source:
+            print(users_list[i])
+        else:
+            print("this doesn't work")
+    
