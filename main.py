19ed86f4a540ec26f39b95199328b5d7c5b859e9
plentakill5h <105579392+plentakill5h@users.noreply.github.com>
2022-06-24 02:59:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,40 @@ import webbrowser
 import selenium
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
 
-phone_number = "+32499136862"
-login_url = "https://web.telegram.org/"
+with open('/home/ubuntu/Desktop/telegram_bot/files/groups.txt','r') as groupsList:
+    groups = groupsList.readlines()
+
+login_url = "https://web.telegram.org/?legacy=1#/login"
 driver = webdriver.Chrome("/usr/bin/chromedriver")
-
 driver.get(login_url)
 
-login = driver.find_element_by_xpath("//*[@id='auth-pages']/div/div[2]/div[2]/div/div[2]/button/div")
-login.click()
 
-#driver.close()
+print("Connecting to Telegram Web, please wait")
+
+phone_input_code = driver.find_element_by_name("phone_country")
+phone_input_number = driver.find_element_by_name("phone_number")
+
+phone_input_code.send_keys(Keys.BACK_SPACE, "32")
+phone_input_number.send_keys("499136862")
+
+time.sleep(3)
+
+next_button = driver.find_element_by_xpath("//*[@id='ng-app']/body/div[1]/div/div[3]/div[1]/div/a")
+next_button.click()
+
+time.sleep(3)
+
+my_number_button = driver.find_element_by_xpath("//*[@id='ng-app']/body/div[4]/div[2]/div/div/div[2]/button[2]")
+my_number_button.click()
+
+time.sleep(3)
+
+enter_code_input = driver.find_element_by_xpath("//*[@id='ng-app']/body/div[1]/div/div[3]/div[2]/form/div[3]/input")
+enter_code = input("Code: ")
+
+time.sleep(50)
+
+enter_code_input.send_keys(enter_code + Keys.ENTER)
